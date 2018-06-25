9ac50a14b873c85c1cfea34da4a0e93fef9d4074
lucky98622 <745478424@qq.com>
2018-06-25 19:19:34 +0800

    后端统合前端

--- BackEnd/src/main/java/com/neu/demo/inter/IndividualRepository.java
+++
@@ -1,7 +0,0 @@
-package com.neu.demo.inter;
-
-import com.neu.demo.model.Individual;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IndividualRepository extends JpaRepository<Individual, Integer> {
-}
