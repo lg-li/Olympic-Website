b6ce6aee9be954bb6524fd0a251e509534831b11
lucky98622 <745478424@qq.com>
2018-06-28 10:47:22 +0800

    首页添加rank注入

--- src/main/java/cn/edu/neu/assignment/controller/FrontEndController.java
+++ src/main/java/cn/edu/neu/assignment/controller/FrontEndController.java
@@ -1,6 +1,9 @@
 package cn.edu.neu.assignment.controller;
 
 import cn.edu.neu.assignment.inter.CompetitionRepository;
+import cn.edu.neu.assignment.inter.DelegationRepository;
+import cn.edu.neu.assignment.model.Competition;
+import cn.edu.neu.assignment.model.Delegation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
@@ -8,15 +11,40 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+
 @Controller
 public class FrontEndController {
 
     @Autowired
     CompetitionRepository competitionRepository;
+    @Autowired
+    DelegationRepository delegationRepository;
 
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("list", competitionRepository.findAll(new PageRequest(0,4)));
+
+        List<Delegation> delegations = delegationRepository.findAll();
+        Collections.sort(delegations, new Comparator<Delegation>(){
+            public int compare(Delegation a1, Delegation a2) {
+                int x = a1.countMedals(1) - a2.countMedals(1);
+                int y = a1.countMedals(2) - a2.countMedals(2);
+                int z = a1.countMedals(3) - a2.countMedals(3);
+                if(x==0){
+                    if(y==0){
+                        return z;
+                    }
+                    return y;
+                }
+                return x;
+            }
+
+        });
+        model.addAttribute("rank",delegations);
         return "index";
     }
 
