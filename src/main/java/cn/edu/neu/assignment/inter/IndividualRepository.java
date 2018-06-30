dae26201e61b8923bf2235c1f406aece6a327de9
lucky98622 <745478424@qq.com>
2018-06-30 21:12:13 +0800

    修复个人及团队懒加载bug

--- src/main/java/cn/edu/neu/assignment/inter/IndividualRepository.java
+++ src/main/java/cn/edu/neu/assignment/inter/IndividualRepository.java
@@ -1,7 +1,14 @@
 package cn.edu.neu.assignment.inter;
 
 import cn.edu.neu.assignment.model.Individual;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IndividualRepository extends JpaRepository<Individual, Integer> {
+    @Override
+    @EntityGraph(value = "individual.findById", type = EntityGraphType.FETCH)
+    Optional<Individual> findById(Integer integer);
 }
